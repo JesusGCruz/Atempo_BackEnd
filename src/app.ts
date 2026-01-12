02d71aed7ec37ba0b871fed337a005bf9ab5b68f
jesus <jgarridocruz5@gmail.com>
2026-01-12 14:21:32 -0600

    Se limpió index y se optimizó la configuración de app

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import babyItemRoutes from './presentation/routes/BabyItemRoutes';
+
+dotenv.config();
+
+const app: Application = express();
+
+// Settings
+app.disable('x-powered-by');
+app.set("port", process.env.PORT || 3000);
+
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas de productos
+app.use('/items', babyItemRoutes);
+
+// Ruta default
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    error: 'Ruta no encontrada xd'
+  });
+});
+
+export default app;
